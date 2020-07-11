a7c48832d0312b65a885fcfbe3884a5b5540f276
Emilio <emrod321@gmail.com>
2020-07-11 16:00:10 -0500

    updated PostListView to list all post in the PostViewModel

--- Birdui/Birdui/PostListView.swift
+++ Birdui/Birdui/PostListView.swift
@@ -9,17 +9,33 @@
 import SwiftUI
 
 struct PostListView: View {
-   
     
-  var body: some View {
-    // TODO: This should look exactly like the Birdie table view,
-    // but with only one button.
-    Text("Layout header, new-post button, List of posts")
-  }
+    @ObservedObject var postVM = PostViewModel()
+    
+    var body: some View {
+        // TODO: This should look exactly like the Birdie table view,
+        // but with only one button.
+        NavigationView {
+            List {
+                ForEach(postVM.posts) { post in
+                    PostView(post: post)
+                }
+            }
+            .navigationBarTitle("Home")
+            .navigationBarItems(leading:
+                Button(action: {
+                    self.postVM.addPost(post: MediaPost(textBody: "I am going to post the chop picture again!!!", userName: "Emilio", timestamp: Date(timeIntervalSinceNow: 0), uiImage: UIImage(named: "chop")))
+                }, label: {
+                    Text("Add New Post")
+                })
+            )
+        }
+        
+    }
 }
 
 struct PostListView_Previews: PreviewProvider {
-  static var previews: some View {
-    PostListView()
-  }
+    static var previews: some View {
+        PostListView()
+    }
 }
