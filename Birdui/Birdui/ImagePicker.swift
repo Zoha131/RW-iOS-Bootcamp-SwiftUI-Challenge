72e432512f223ad9c7e0fc798f7f407eb1831e99
zoha131 <abirhasanzoha@gmail.com>
2020-07-11 17:06:22 +0600

    Added ImagePicker Sheet

--- Birdui/Birdui/ImagePicker.swift
+++ Birdui/Birdui/ImagePicker.swift
@@ -8,5 +8,53 @@
 
 import SwiftUI
 
-// Create struct ImagePicker: UIViewControllerRepresentable { }
+struct ImagePicker: UIViewControllerRepresentable {
+  
+  
+  @Binding var uiImage: UIImage?
+  @Environment(\.presentationMode) var presentationMode
+  
+  class Coordinator: NSObject,
+    UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate{
+    
+    var parent: ImagePicker
+    
+    init(_ parent: ImagePicker){
+      self.parent = parent
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+      
+      if let uiImage = info[.originalImage] as? UIImage{
+        parent.uiImage = uiImage
+      }
+      
+      parent.presentationMode.wrappedValue.dismiss()
+    }
+    
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(self)
+  }
+  
+  func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) ->  UIImagePickerController {
+    let picker = UIImagePickerController()
+    picker.delegate = context.coordinator
+    return picker
+  }
+  
+  func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+    
+  }
+  
+  
+}
 // Hint: hackingwithswift.com
+
+struct ImagePicker_Previews: PreviewProvider {
+  static var previews: some View {
+    /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+  }
+}
--- Birdui/Birdui/NewPostView.swift
+++ Birdui/Birdui/NewPostView.swift
@@ -51,7 +51,7 @@ struct NewPostView: View {
     }
     .sheet(isPresented: $showImagePicker) {
       // TODO: Show ImagePicker
-      Text("Replace with code to show ImagePicker")
+      ImagePicker(uiImage: self.$uiImage)
     }
   }
 }
